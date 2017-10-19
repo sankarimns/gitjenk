86b90549fb153ddcb400e90758081efe3db3f178
mrishnamoorthy <mrishnamoorthy@lm-maa-26500992.paypalcorp.com>
2017-10-19 05:30:42 -0700

    firsttime

---
+++ demojenkgit/src/test/java/demojenkgit/Prog1.java
@@ -0,0 +1,12 @@
+package demojenkgit;
+
+import org.testng.annotations.Test;
+
+@Test (priority = 1)
+public class Prog1 {
+
+	public void method1() {
+		System.out.println("Helo world1");
+	}
+	
+}
---
+++ demojenkgit/src/test/java/demojenkgit/Prog2.java
@@ -0,0 +1,13 @@
+package demojenkgit;
+
+import org.testng.annotations.Test;
+
+@Test(priority = 2)
+public class Prog2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello world2");
+	}
+
+}
