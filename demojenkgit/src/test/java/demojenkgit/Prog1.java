d85bf8dd7af8b39309454329e9151b95091fb29b
mrishnamoorthy <mrishnamoorthy@lm-maa-26500992.paypalcorp.com>
2017-10-19 23:05:23 -0700

    second

--- demojenkgit/src/test/java/demojenkgit/Prog1.java
+++ demojenkgit/src/test/java/demojenkgit/Prog1.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 @Test (priority = 1)
 public class Prog1 {
 
-	public void method1() {
+	public static void main(String[] args) {
 		System.out.println("Helo world1");
 	}
 	
